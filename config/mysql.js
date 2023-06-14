b58a4a4b48deb730976298b7dec5933dfa5bdc22
suzel <64069582+suzelkhan1177@users.noreply.github.com>
2023-06-14 14:16:32 -0400

    fixed error

--- config/mysql.js
+++ config/mysql.js
@@ -2,23 +2,48 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 const db = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASS,
-    database: process.env.DATABASE,
-  });
-  
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASS,
+  database: process.env.DATABASE,
+});
+
+db.on('error', (err) => {
+  if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+    console.error('MySQLdb lost. Reconnecting...');
+    handleReconnect();
+  } else {
+    throw err;
+  }
+});
+
+// Establish the initialdb
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+
+  } else {
+    console.log('Connected to MySQL server');
+
+  }
+});
+
+// Example reconnect logic
+const handleReconnect = () => {
+  db.end();
+
   db.connect((err) => {
     if (err) {
-      throw err;
+      console.error('Error reconnecting to MySQL:', err);
+
+    } else {
+      console.log('Reconnected to MySQL server');
     }
-    console.log('Connected to the MySQL database');
   });
-  db.query('select 1 + 1', (err, rows) => { /* */ });
+};
 
-
-  const createTables = () => {
-    const userTableQuery = `
+const createTables = () => {
+  const userTableQuery = `
       CREATE TABLE User (
         id INT PRIMARY KEY AUTO_INCREMENT,
         profilepic VARCHAR(255),
@@ -28,8 +53,8 @@ const db = mysql.createConnection({
         modified DATETIME
       )
     `;
-  
-    const blogTableQuery = `
+
+  const blogTableQuery = `
     CREATE TABLE Blog (
       blogid INT PRIMARY KEY AUTO_INCREMENT,
       blogimage VARCHAR(255),
@@ -42,25 +67,25 @@ const db = mysql.createConnection({
       FOREIGN KEY (userId) REFERENCES User(id)
     )
     `;
-  
-    db.query(userTableQuery, (error) => {
-      if (error) {
-        console.error('Table Already created');
-      } else {
-        console.log('User table created successfully.');
-      }
-    });
-  
-    db.query(blogTableQuery, (error) => {
-      if (error) {
-        console.error('Table Already created');
-      } else {
-        console.log('Blog table created successfully.');
-      }
-    });
-  };
-  
-  createTables();
-  
 
-  module.exports = db;
+  db.query(userTableQuery, (error) => {
+    if (error) {
+      console.error('Table Already created');
+    } else {
+      console.log('User table created successfully.');
+    }
+  });
+
+  db.query(blogTableQuery, (error) => {
+    if (error) {
+      console.error('Table Already created');
+    } else {
+      console.log('Blog table created successfully.');
+    }
+  });
+};
+
+createTables();
+
+
+module.exports = db;
